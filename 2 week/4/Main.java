804a24e284059f6bb6f8141bb099e6b36a312b50
DanyaLNS <92125582+DanyaLNS@users.noreply.github.com>
2021-10-27 13:37:45 +0300

    Поправил первую задачу со 2 недели

--- 2 week/4/Main.java
+++ 2 week/4/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Annotation annotation1 = new Annotation("Car", "Rectangle");
-        Annotation annotation2 = new Annotation("Moon", "Circle");
+        Annotation annotation1 = new Annotation("Car", "rectangle");
+        Annotation annotation2 = new Annotation("Moon", "circle");
 
         AnnotatedImage annotatedImage = new AnnotatedImage("", annotation1, annotation2);
+        // Метод для вывода всех аннотаций
         annotatedImage.printAnnotations();
     }
 }
