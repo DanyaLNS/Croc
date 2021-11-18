9d1642dddb8413bba814628033d8451e87c220ca
DanyaLNS <92125582+DanyaLNS@users.noreply.github.com>
2021-11-18 14:10:54 +0300

    Поправил 11 задачу

--- 5 week/11/Lot.java
+++ 5 week/11/Lot.java
@@ -1,44 +1,32 @@
-
 import java.util.Scanner;
 
-public class Lot extends Thread {
+public class Lot {
     private static final Object lock = new Object();
     private volatile int currentPrice;
     private volatile String buyerName;
-    private volatile int currentTime, endTime;
+    private volatile long endTime;
 
-    @Override
-    public synchronized void run() {
-        while (currentTime < endTime) {
-            synchronized (lock) {
-                System.out.println("Введите новую цену и имя покупателя:");
-                Scanner in = new Scanner(System.in);
-                int newPrice;
-                String newBuyer;
-                newPrice = in.nextInt();
-                newBuyer = in.nextLine();
-                bet(newPrice, newBuyer, currentTime);
-                currentTime++;
-                in.close();
-            }
-        }
-        Thread.currentThread().interrupt();
-    }
 
-    public Lot(int startPrice, int endTime) {
+    public Lot(int startPrice, long endTime) {
         currentPrice = startPrice;
         this.endTime = endTime;
         currentTime = 0;
     }
 
-    public void bet(int price, String buyer, int currentTime) {
+    public void bet(int price, String buyer, long currentTime) {
         if (currentTime < endTime && price > currentPrice) {
-            currentPrice = price;
-            buyerName = buyer;
+            synchronized (lock) {
+                if (currentTime < endTime && price > currentPrice) {
+                    currentPrice = price;
+                    buyerName = buyer;
+                }
+            }
         }
     }
 
     public String getWinner() {
-        return buyerName;
+        synchronized (lock) {
+            return buyerName;
+        }
     }
 }
