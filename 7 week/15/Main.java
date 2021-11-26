391d94f37122ecc6ff80f3246a806f4961621f21
DanyaLNS <92125582+DanyaLNS@users.noreply.github.com>
2021-11-26 11:40:35 +0300

    Решение 15 задачи

---
+++ 7 week/15/Main.java
@@ -0,0 +1,21 @@
+package Croc.work15;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        RemoveComments removeComments = new RemoveComments();
+        List<String> comments = new LinkedList<>();
+        comments.add("Я не против убить его");
+        comments.add("Тебя повесить мало))0)");
+        comments.add("Вау! классно!!!");
+        comments.add("Тебя повесить мало))0)");
+        comments.add("Тебя повесить мало))0)");
+        comments.add("Стоит более детально изучить тему...");
+        comments.add("Я не против убить его");
+        Predicate<String> predicate = s -> (s.indexOf("убить") != -1) || (s.indexOf("повесить") != -1);
+        System.out.println(removeComments.filterComments(predicate,comments));
+    }
+}
